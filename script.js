8c3d7bd47ceece7ad59751eb33f695f1c47bd846
Cherry Ng <cherry92@gmail.com>
2020-05-10 22:53:09 +0800

    Auto redirect to zh-hk in case of Simplified Chinese.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const pathname = window.location.pathname;
+
+if (pathname.indexOf('/wiki/') == 0 || pathname.indexOf('/zh-hans/') == 0 || pathname.indexOf('/zh-cn/') == 0 || pathname.indexOf('/zh-sg/') == 0 || pathname.indexOf('/zh-my/') == 0) {
+  let segments = pathname.split('/');
+  segments[1] = 'zh-hk';
+  window.location.pathname = segments.join('/');
+}
+
 function makeNoConvertInTc() {
   const link = document.querySelector('li[id^="ca-varlang-"] a[hreflang="zh"]');
   link.textContent = '不轉換';
@@ -15,7 +23,7 @@ function cleanVariantsMenu() {
   });
 }
 
-window.setInterval(function() {
+window.setTimeout(function() {
   makeNoConvertInTc();
   cleanVariantsMenu();
-}, 1000 / 17);
+}, 1000);
